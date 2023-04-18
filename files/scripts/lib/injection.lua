43faa57a78e1e280dd61d5610109f06381a8425c
Nahtan <nathan.titirangi@gmail.com>
2023-04-18 15:59:22 +1200

    code cleanliness
    using copi code standard

--- files/scripts/lib/injection.lua
+++ files/scripts/lib/injection.lua
@@ -5,50 +5,61 @@
 
 local ENABLE_LOGGING = false -- change this thing if you want to spam the console
 
+function log(log_table)
+	print(table.concat(log_table))
+end
+
 function append(file, target, text)
 	local content = ModTextFileGetContent(file)
 	local first, last = content:find(target, 0, true)
+	if not first then
+		log({ "INJECTION (APPEND) FAILED: NO HOOK\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
+		return
+	end
 	local before = content:sub(1, last)
 	local after = content:sub(last + 1)
 	local new = before .. text .. after
 	ModTextFileSetContent(file, new)
 	if ENABLE_LOGGING then
-		print("Injected (append) " ..
-			text .. " in " .. file .. " at " .. target .. " causing:\n" .. new .. "\nfrom: \n" .. content)
+		log({ "Injected (append) ", text, " in ", file, " at ", target, " causing:\n", new, "\nfrom: \n", content })
 	end
 	if content == new then
-		print("INJECTION (APPEND) FAILED:\nFile: " .. file .. "\nTarget: " .. target ..
-			"\nText: " .. text)
+		log({ "INJECTION (APPEND) FAILED: NO CHANGE\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
 	end
 end
 
 function prepend(file, target, text)
 	local content = ModTextFileGetContent(file)
 	local first, last = content:find(target, 0, true)
+	if not first then
+		log({ "INJECTION (PREPEND) FAILED: NO HOOK\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
+		return
+	end
 	local before = content:sub(1, first - 1)
 	local after = content:sub(first)
 	local new = before .. text .. after
 	ModTextFileSetContent(file, new)
-	print("Injected (prepend) " ..
-		text .. " in " .. file .. " at " .. target .. " causing:\n" .. new .. "\nfrom: \n" .. content)
+
+	log({ "Injected (prepend) ", text, " in ", file, " at ", target, " causing:\n", new, "\nfrom: \n", content })
 	if content == new then
-		print("INJECTION (PREPEND) FAILED:\nFile: " .. file .. "\nTarget: " ..
-			target .. "\nText: " .. text)
+		log({ "INJECTION (PREPEND) FAILED: NO CHANGE\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
 	end
 end
 
 function replace(file, target, text)
 	local content = ModTextFileGetContent(file)
 	local first, last = content:find(target, 0, true)
+	if not first then
+		log({ "INJECTION (REPLACE) FAILED: NO HOOK\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
+		return
+	end
 	local before = content:sub(1, first - 1)
 	local after = content:sub(last + 1)
 	local new = before .. text .. after
 	ModTextFileSetContent(file, new)
-	print("Injected (replace) " ..
-		text .. " in " .. file .. " at " .. target .. " causing:\n" .. new .. "\nfrom: \n" .. content)
+	log({ "Injected (replace) ", text, " in ", file, " at ", target, " causing:\n", new, "\nfrom: \n", content })
 	if content == new then
-		print("INJECTION (REPLACE) FAILED:\nFile: " .. file .. "\nTarget: " ..
-			target .. "\nText: " .. text)
+		log({ "INJECTION (REPLACE) FAILED: NO CHANGE\nFile: ", file, "\nTarget: ", target, "\nText: ", text })
 	end
 end
 
