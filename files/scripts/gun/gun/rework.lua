7c778fbf52faf3f35ced0503b743f0697f7718ff
Nahtan <nathan.titirangi@gmail.com>
2023-04-20 15:05:26 +1200

    nerfed triggers
    they now slow down when spells in them

--- files/scripts/gun/gun/rework.lua
+++ files/scripts/gun/gun/rework.lua
@@ -5,3 +5,4 @@ prepend_from_file("data/scripts/gun/gun.lua","mods/community_rebalance/files/scr
 replace("data/scripts/gun/gun.lua","draw_shot( create","draw_shot_trigger( create")
 replace("data/scripts/gun/gun.lua","draw_shot( create","draw_shot_trigger( create")
 replace("data/scripts/gun/gun.lua","draw_shot( create","draw_shot_trigger( create")
+append("data/scripts/gun/gun.lua","action.action()","\nif trigger_nesting~=0 then\ncounter[trigger_nesting] = counter[trigger_nesting] + 1\nend\n")
